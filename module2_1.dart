f96dc959a3090a18bbb83aeac461c82751173264
Phix0808 <76470581+Phix0808@users.noreply.github.com>
2022-05-19 15:43:44 +0200

    Add files via upload

---
+++ module2_1.dart
@@ -0,0 +1,11 @@
+import 'package:module2_1/module2_1.dart' as module2_1;
+
+void main() {
+  var name = "Philile Hlalukana";
+  var favouriteApp = "Instagram";
+  var city = "Randfontein";
+
+  print('Name: $name');
+  print('Favourite App: $favouriteApp');
+  print('Name: $city');
+}
